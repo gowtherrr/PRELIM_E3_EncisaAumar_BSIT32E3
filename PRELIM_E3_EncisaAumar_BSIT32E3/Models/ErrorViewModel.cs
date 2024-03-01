4721bdb4a306785623661b05079265c01a04df19
AUMAR TRISTAN B. ENCISA <aumarencisa16@gmail.com>
2024-03-01 22:50:45 +0800

    Add project files.

---
+++ PRELIM_E3_EncisaAumar_BSIT32E3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRELIM_E3_EncisaAumar_BSIT32E3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
